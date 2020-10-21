909a19cec14594a2531140dd0987a266845ce61d
Andy Lian <andylke@gmail.com>
2020-10-21 23:36:54 +0800

    Add DiscoveryClient support.

--- src/main/java/io/getstarted/spring/Application.java
+++ src/main/java/io/getstarted/spring/Application.java
@@ -2,10 +2,12 @@ package io.getstarted.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class Application {
 
   public static final void main(final String[] args) {
